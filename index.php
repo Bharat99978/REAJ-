1089bd53b465b7302d4339941b53110b23916850
Bharat99978 <166504281+Bharat99978@users.noreply.github.com>
2024-11-28 19:31:42 +0530

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "Web hosting is working!";
-echo "<br>Database Connection Info:";
-echo "<br>Host: mysql-db";
-echo "<br>Database: my_database";
-echo "<br>User: my_user";
-echo "<br>Password: [Check .env file]";
-?>
